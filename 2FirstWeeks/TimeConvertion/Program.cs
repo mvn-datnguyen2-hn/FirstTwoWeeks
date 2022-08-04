971612df9b1371bc9c60742b86fbdef6a8f70eaf
Dat Nguyen Tien <datntph17302@fpt.edu.vn>
2022-08-04 16:25:26 +0700

    Bai 2

---
+++ 2FirstWeeks/TimeConvertion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConvertion
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string timeConversion(string s)
+            {
+                var amOrPm = s.Substring(8);
+                var hour = s.Substring(0, 2);
+                var minuteSecond = s.Substring(2, 6);
+                if (amOrPm == "AM" && hour == "12")
+                {
+                    hour = "00";
+                }
+                else if (amOrPm == "PM")
+                {
+                    var numericHourComponent = int.Parse(hour);
+                    if (numericHourComponent != 12)
+                    {
+                        hour = Convert.ToString(12 + numericHourComponent);
+                    }
+                }
+                return (hour + minuteSecond);
+            }
+            string gio = Console.ReadLine();
+
+            string result = timeConversion(gio);
+        }
+    }
+}
