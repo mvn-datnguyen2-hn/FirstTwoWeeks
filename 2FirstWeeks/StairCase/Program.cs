6cb9c153e31a35a5488112e8d4692ad47c43f1de
Dat Nguyen Tien <datntph17302@fpt.edu.vn>
2022-08-04 16:58:36 +0700

    Bai3

---
+++ 2FirstWeeks/StairCase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StairCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new String(' ', n - i) + new String('#', i));
+            }
+        }
+    }
+}
