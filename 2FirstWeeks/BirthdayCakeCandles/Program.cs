23dc20ef9b7057ed2a921c312d5a768e14d66602
Dat Nguyen Tien <datntph17302@fpt.edu.vn>
2022-08-04 17:35:01 +0700

    Bai 9

---
+++ 2FirstWeeks/BirthdayCakeCandles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayCakeCandles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+            int candle = 0;
+
+            List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+            int max = ar.Max();
+            for (int i = 0; i < arCount; i++)
+            {
+                if (ar[i] == max)
+                {
+                    candle++;
+                }
+            }
+            Console.WriteLine(candle);
+        }
+    }
+}
