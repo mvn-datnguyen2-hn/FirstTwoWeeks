8ca9ac1da4248389d7f270f79b5b7391400649a6
Dat Nguyen Tien <datntph17302@fpt.edu.vn>
2022-08-05 16:08:21 +0700

    Records

---
+++ 2FirstWeeks/Records/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Records
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              List<int> BreakingRecords(List<int> scores)
+            {
+                int win = 0;
+                int lose = 0;
+                int newRecord = 0;
+                int low = 0;
+                for (int i = 0; i < scores.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        newRecord = scores[0];
+                        low = scores[0];
+                    }
+                    else
+                    {
+                        if (scores[i] > newRecord)
+                        {
+                            win++;
+                            newRecord = scores[i];
+                        }
+                        if (scores[i] < low)
+                        {
+                            lose++;
+                            low = scores[i];
+                        }
+                    }
+
+                }
+                List<int> count = new List<int>() { win, lose };
+                return count;
+            }
+              int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+              List<int> score = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+              List<int> result = BreakingRecords(score);
+
+              Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
