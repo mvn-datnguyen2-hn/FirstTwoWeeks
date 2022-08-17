fe514475ed0790267d216951e9841c4c434ef0df
Dat Nguyen Tien <datntph17302@fpt.edu.vn>
2022-08-17 13:09:15 +0700

    BirthdayCakeCandles

--- 2FirstWeeks/BirthdayCakeCandles/Program.cs
+++ 2FirstWeeks/BirthdayCakeCandles/Program.cs
@@ -8,21 +8,21 @@ namespace BirthdayCakeCandles
 {
     internal class Program
     {
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            int max = candles.Max();
+            int candle = candles.FindAll(c => c == max).Count;
+            return candle;
+        }
         static void Main(string[] args)
         {
-            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
-            int candle = 0;
+            int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
 
-            List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
-            int max = ar.Max();
-            for (int i = 0; i < arCount; i++)
-            {
-                if (ar[i] == max)
-                {
-                    candle++;
-                }
-            }
-            Console.WriteLine(candle);
+            List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+            int result = birthdayCakeCandles(candles);
+
+            Console.WriteLine(result);
         }
     }
 }
