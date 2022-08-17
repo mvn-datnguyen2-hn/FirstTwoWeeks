0bc3ec227f92458ffd3d3e6781c0c1ea436abd91
Dat Nguyen Tien <datntph17302@fpt.edu.vn>
2022-08-17 12:48:37 +0700

    PlusMinus

--- 2FirstWeeks/PlusMinus/Program.cs
+++ 2FirstWeeks/PlusMinus/Program.cs
@@ -8,32 +8,19 @@ namespace PlusMinus
 {
     internal class Program
     {
+        private static void plusMinus(List<int> arr)
+        {
+            decimal pos = arr.FindAll(c => c > 0).Count;
+            decimal neg = arr.FindAll(c => c < 0).Count;
+            decimal zero = arr.FindAll(c => c == 0).Count;
+
+            Console.WriteLine((pos / arr.Count).ToString("N6"));
+            Console.WriteLine((neg / arr.Count).ToString("N6"));
+            Console.WriteLine((zero / arr.Count).ToString("N6"));
+        }
         static void Main(string[] args)
         {
-            void plusMinus(List<int> arr)
-            {
-                decimal pos = 0;
-                decimal neg = 0;
-                decimal zero = 0;
-                for (int i = 0; i < arr.Count; i++)
-                {
-                    if (arr[i] < 0)
-                    {
-                        neg += 1;
-                    }
-                    if (arr[i] > 0)
-                    {
-                        pos += 1;
-                    }
-                    if (arr[i] == 0)
-                    {
-                        zero += 1;
-                    }
-                }
-                Console.WriteLine((pos / arr.Count).ToString("N6"));
-                Console.WriteLine((neg / arr.Count).ToString("N6"));
-                Console.WriteLine((zero / arr.Count).ToString("N6"));
-            }
+            
             int n = Convert.ToInt32(Console.ReadLine().Trim());
 
             List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
