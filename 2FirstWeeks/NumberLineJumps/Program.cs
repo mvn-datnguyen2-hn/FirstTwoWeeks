9b5dd8ca68d50d35b4046eb1c736661bde6f583c
Dat Nguyen Tien <datntph17302@fpt.edu.vn>
2022-08-18 10:31:40 +0700

    NumberLineJumps

--- 2FirstWeeks/NumberLineJumps/Program.cs
+++ 2FirstWeeks/NumberLineJumps/Program.cs
@@ -8,45 +8,28 @@ namespace NumberLineJumps
 {
     internal class Program
     {
+        private static string Kangaroo(long x1, long v1, long x2, long v2)
+        {
+            string a;
+            if ((x1 < x2 && v1 < v2) || (x2 < x1 && v2 < v1))//ở xa hơn,tốc độ kém hơn => k thể kịp
+            {
+                return "NO";
+            }
+            //ở xa hơn nhưng chỉ cần tốc độ cao hơn => chắc chắn sẽ kịp
+            var numberOfJumps = Math.Abs(((double)(x1 - x2)) / (v2 - v1));
+
+            if (numberOfJumps % 1 == 0)
+                a = "YES";
+            else
+            {
+                a = "NO";
+            }
+            return a;
+
+        }
         static void Main(string[] args)
         {
-              string Kangaroo(long x1, long v1, long x2, long v2)
-            {
-                string a;
-                if ((x1 < x2 && v1 < v2) || (x2 < x1 && v2 < v1))//ở xa hơn,tốc độ kém hơn => k thể kịp
-                {
-                    return "NO";
-                }
-                //ở xa hơn nhưng chỉ cần tốc độ cao hơn => chắc chắn sẽ kịp
-                else if (x2 < x1)
-                {
-
-                    var numberOfJumps = ((double)(x1 - x2)) / (v2 - v1);
-
-                    if (numberOfJumps % 1 == 0)
-                        a = "YES";
-                    else
-                    {
-                        a = "NO";
-                    }
-
-                }
-                else
-                {
-                    //v1 > v2
-                    var numberOfJumps = ((double)(x2 - x1)) / (v1 - v2);
-                    //check whether number of jumps is a whole number  i.e no fractional part.
-                    if (numberOfJumps % 1 == 0)
-                        a = "YES";
-                    else
-                    {
-                        a = "NO";
-                    }
-
-                }
-                return a;
-               
-            }
+              
               string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
 
               long xO = Convert.ToInt32(firstMultipleInput[0]);
