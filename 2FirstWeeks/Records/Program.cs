1e0ca586c0fd8ef315cdda0f230bbaa10efde7f9
Dat Nguyen Tien <datntph17302@fpt.edu.vn>
2022-08-18 10:19:54 +0700

    Records

--- 2FirstWeeks/Records/Program.cs
+++ 2FirstWeeks/Records/Program.cs
@@ -8,39 +8,38 @@ namespace Records
 {
     internal class Program
     {
+        private static List<int> BreakingRecords(List<int> scores)
+        {
+            int win = 0;
+            int lose = 0;
+            int newRecord = 0;
+            int low = 0;
+            for (int i = 0; i < scores.Count; i++)
+            {
+                if (i == 0)
+                {
+                    newRecord = scores[0];
+                    low = scores[0];
+                }
+                else
+                {
+                    if (scores[i] > newRecord)
+                    {
+                        win++;
+                        newRecord = scores[i];
+                    }
+                    if (scores[i] < low)
+                    {
+                        lose++;
+                        low = scores[i];
+                    }
+                }
+            }
+            return new List<int>() { win, lose }; ;
+        }
         static void Main(string[] args)
         {
-              List<int> BreakingRecords(List<int> scores)
-            {
-                int win = 0;
-                int lose = 0;
-                int newRecord = 0;
-                int low = 0;
-                for (int i = 0; i < scores.Count; i++)
-                {
-                    if (i == 0)
-                    {
-                        newRecord = scores[0];
-                        low = scores[0];
-                    }
-                    else
-                    {
-                        if (scores[i] > newRecord)
-                        {
-                            win++;
-                            newRecord = scores[i];
-                        }
-                        if (scores[i] < low)
-                        {
-                            lose++;
-                            low = scores[i];
-                        }
-                    }
-
-                }
-                List<int> count = new List<int>() { win, lose };
-                return count;
-            }
+             
               int n = Convert.ToInt32(Console.ReadLine().Trim());
 
               List<int> score = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
